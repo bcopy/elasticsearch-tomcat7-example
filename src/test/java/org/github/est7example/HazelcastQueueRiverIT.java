5d54c0ebfbebdda31eb27cc35f82db92b0c4a5ee
brice.copy <brice.copy@gmail.com>
2014-06-15 16:26:49 +0200

    Fixed various references - note that the integration test does not work (cannot join local Tomcat ES instance)

--- src/test/java/org/github/est7example/HazelcastQueueRiverIT.java
+++ src/test/java/org/github/est7example/HazelcastQueueRiverIT.java
@@ -1,64 +1,73 @@
 package org.github.est7example;
 
+import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
 import static org.junit.Assert.*;
 
+import java.util.Map;
 import java.util.Properties;
 
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.node.Node;
 import org.junit.Test;
 
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.ITopic;
-import com.hazelcast.monitor.LocalTopicStats;
 
 public class HazelcastQueueRiverIT {
 
 	@Test
-	public void testSimpleTopic() throws Exception{
-		
+	public void testSimpleTopic() throws Exception {
+
 		Properties hazelcastProps = new Properties();
-		hazelcastProps.load(getClass().getClassLoader().getResource(
-				"hazelcast.properties").openStream());
-		ClientConfig clientConfig = new ClientConfig()
-				.addAddress("127.0.0.1:5701");
-		(clientConfig
-				.getGroupConfig()).setName(
-						hazelcastProps
-								.getProperty("hazelcast.cluster.group.name"))
-				.setPassword(
-						hazelcastProps
-								.getProperty("hazelcast.cluster.group.password"));
+		hazelcastProps.load(getClass().getClassLoader()
+				.getResource("hazelcast.properties").openStream());
+		ClientConfig clientConfig = new ClientConfig();
+		clientConfig.getNetworkConfig().addAddress("127.0.0.1:5701");
+		// (clientConfig
+		// .getGroupConfig()).setName(
+		// hazelcastProps
+		// .getProperty("hazelcast.cluster.group.name"))
+		// .setPassword(
+		// hazelcastProps
+		// .getProperty("hazelcast.cluster.group.password"));
 
 		HazelcastInstance hzClient = HazelcastClient
 				.newHazelcastClient(clientConfig);
-		
+
 		TestMessage testMessage = new TestMessage();
-		ITopic<TestMessage> topic = hzClient.getTopic("test-topic");
+		ITopic<String> topic = hzClient.getTopic("test-topic");
+
+		long startAt = System.currentTimeMillis();
+		for (int i = 0; i < 100000; i++) {
+			topic.publish(testMessage.toString());
+		}
 		
+		System.out.println("Publishing took "+(System.currentTimeMillis()-startAt)+" ms");
+
+		// LocalTopicStats lts = topic.getLocalTopicStats();
+		// assertTrue(lts.getReceiveOperationCount() > 0);
+        
+		// Wait for indexing to take place
+		Thread.sleep(40000);
+//		hzClient.shutdown();
 		
-		topic.publish(testMessage);
-		topic.publish(testMessage);
-		topic.publish(testMessage);
-		
-//		LocalTopicStats lts = topic.getLocalTopicStats();
-//		assertTrue(lts.getReceiveOperationCount() > 0);
-		
-        // Wait for indexing to take place
-		Thread.sleep(4000);
-		
+		System.out.println("Completed.");
 		// Query elasticsearch
-//		Node node = nodeBuilder().node();
+//		Node node = nodeBuilder().clusterName("elasticsearch-tomcat7-example").data(false).node();
 //		Client esClient = node.client();
 //		
-//		GetResponse gr = esClient.prepareGet("hazelcast-test", "hazelcast-message", testMessage.getUuid().toString()).execute().actionGet();
-//		Map<String, Object> testMessageMap= gr.getSourceAsMap();
-//		
+//
+//		GetResponse gr = esClient
+//				.prepareGet().setIndex("hazelcast-test").setType("test-topic").execute().actionGet();
+//		Map<String, Object> testMessageMap = gr.getSourceAsMap();
+//
 //		assertNotNull(testMessageMap);
-//		
+//
 //		node.close();
-		
-		
+
 	}
 
 }
